284af86d18803e62c18f040dc4885ef51b048537
yowainwright <yowainwright@gmail.com>
2022-02-04 00:02:21 -0800

    feat: adds more tests

--- src/__tests__/mini-cookies.test.ts
+++ src/__tests__/mini-cookies.test.ts
@@ -1,6 +1,19 @@
 import miniCookies from '../mini-cookies'
 
 describe('MiniCookies', () => {
-	it('jest works', () => expect(1).toBe(1))
 	it('miniCookies is defined', () => expect(miniCookies).toBeDefined())
+
+	it('gets cookies', () => {
+		document.cookie = 'foo=bar;'
+		expect(miniCookies().get('foo')).toBe('bar')
+	})
+
+	it('sets cookies', () => {
+		expect(miniCookies().set('biz', 'baz').get('biz')).toBe('baz')
+	})
+
+	it('removes a cookie', () => {
+		expect(miniCookies().set('fiz', 'buz').get('fiz')).toBe('buz')
+		expect(miniCookies().remove('fiz').get('fiz')).toBeUndefined()
+	})
 })
