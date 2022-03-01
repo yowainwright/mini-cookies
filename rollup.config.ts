3b74e44ebbcef7f05e2b3693da8f08e0c3c0f2f4
yowainwright <yowainwright@gmail.com>
2022-02-28 16:16:30 -0800

    fix: add rollup.config.ts

---
+++ rollup.config.ts
@@ -0,0 +1,44 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { uglify } from "rollup-plugin-uglify";
+
+import {
+  author,
+  description,
+  homepage,
+  license,
+  name,
+  version,
+} from "./package.json";
+
+const banner = `/**
+  ${name} - ${description}
+  @version v${version}
+  @link ${homepage}
+  @author ${author}
+  @license ${license}
+**/`;
+
+const plugins = [
+  resolve(),
+  commonjs(),
+  typescript({
+    tsconfig: false,
+    lib: ["esnext", "dom"],
+    target: "esnext",
+    moduleResolution: "node",
+    resolveJsonModule: true,
+  }),
+  uglify(),
+];
+
+export default {
+  input: "src/mini-cookies.ts",
+  output: {
+    file: "dist/mini-cookies.umd.js",
+    format: "umd",
+    name: "miniCookies",
+  },
+  plugins,
+};
