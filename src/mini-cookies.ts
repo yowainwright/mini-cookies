9774c81ce3a81292216976996025ee350e4d6c24
Jeff Wainwright <yowainwright@gmail.com>
2022-04-03 22:29:47 -0700

    feat: adds more attribute spec (#10)
    * feat: adds more attribute spec
    
    * fix: updates CookieAttributes types
    
    * feat: updates cookie attribute types/support + descs

--- src/mini-cookies.ts
+++ src/mini-cookies.ts
@@ -1,6 +1,47 @@
-import { CookieAttributes, CookieDictionary, CookiesOptions } from "./types";
+import { CookieAttributes, CookieDictionary, Options } from "./types";
 
-export default function miniCookies(options: CookiesOptions = {}) {
+/**
+ * Mini Cookies 🍪
+ * @description Mini Cookies is a simple and minimalistic cookie management tool.
+ */
+
+/**
+ * setCookieAttributes
+ * @description a cookie attribute manager
+ * @param {CookieAttributes} attrs cookie attributes
+ * @returns {string}
+ */
+export function setCookieAttributes(attrs: CookieAttributes): string {
+  const items = Object.keys(attrs);
+  // prefer days over expires attribute
+  const filteredAttrs = items.every((attr) =>
+    ["days", "expires"].includes(attr)
+  )
+    ? items.filter((attr) => attr !== "expires")
+    : items;
+  // return attributes with booleans  and key value pairs
+  return filteredAttrs.reduce((str, attr) => {
+    const isTruthyAttr = ["secure", "httponly", "__Secure-"].includes(attr);
+    // return truthy attributes w/o values
+    if (isTruthyAttr) str += `; ${attr}`;
+    // return days helper as expires
+    else if (attr === "days")
+      str += `; expires=${new Date(
+        Date.now() + (attrs[attr] as number) || 0 * 864e5
+      ).toUTCString()};`;
+    // return all other key value string pairs
+    else str += `; ${attr}=${attrs[attr as keyof CookieAttributes]}`;
+    return str;
+  }, "");
+}
+
+/**
+ * mini-cookies 🍪
+ * @description a just a few lines of code cookie manager
+ * @param {Options} options no options yet
+ * @returns {MiniCookies} a cookie manager factory function
+ */
+export default function miniCookies(options: Options = {}) {
   return {
     setCookieList(): CookieDictionary {
       return document.cookie
@@ -14,21 +55,21 @@ export default function miniCookies(options: CookiesOptions = {}) {
           {}
         );
     },
+    // returns a cookie value if available
     get(name: string) {
       const cookies = this.setCookieList();
       if (cookies[name]) {
         return cookies[name];
-      } else if (options.isDebugging) {
-        return console.warn(`Cookie "${name}" not found.`);
       }
     },
-    set(name: string, value: string, { days }: CookieAttributes = {}) {
-      const expires: string = days
-        ? ` expires=${new Date(Date.now() + days * 864e5).toUTCString()};`
-        : "";
-      document.cookie = `${name}=${encodeURIComponent(value)};${expires}`;
+    // sets a cookie with attributes
+    set(name: string, value: string, attrs: CookieAttributes = {}) {
+      document.cookie = `${name}=${encodeURIComponent(
+        value
+      )};${setCookieAttributes(attrs)}`;
       return this;
     },
+    // removes a cookie by setting it's expires attribute to -1 and value to empty
     remove(name: string) {
       this.set(name, "", { days: -1 });
       return this;
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,18 @@
-export type CookiesOptions = {
-	isDebugging?: boolean
-}
-
 export type CookieDictionary = {
-	[key: string]: string
-}
+  [key: string]: string;
+};
 
 export type CookieAttributes = {
-	days?: number
-	path?: string
-	domain?: string
-	secure?: boolean
-}
+  days?: number; // ex, 7
+  domain?: string; // ex "example.com"
+  expires?: Date; // ex, new Date(Date.now())
+  ["__Host-"]?: string;
+  httponly?: boolean;
+  ["max-age"]?: string; // ex, -1
+  path?: string; // ex, "/foo/bar"
+  samesite?: "lax" | "strict" | "none"; // lax: user tracking level, strick: prevents cross-site cookie sending
+  secure?: boolean;
+  ["__Secure-"]?: boolean;
+};
+
+export type Options = Record<string, unknown>;
