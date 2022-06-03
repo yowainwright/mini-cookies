465222b74656eabe3bf8677633a3c29f29303b11
yowainwright <yowainwright@gmail.com>
2022-06-03 11:37:03 -0700

    feat: init page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import miniCookies from 'mini-cookies'
 
 export function CookieEmoji() {
-	return <figure className="mc_cookie-emoji">🍪</figure>
+	return <figure className="mc__img mc__img--cookie-emoji">🍪</figure>
 }
 
 export function AddCookieForm() {
@@ -13,19 +13,31 @@ export function AddCookieForm() {
 		watch,
 		formState: { errors },
 	} = useForm()
-	const onSubmit = (data) => console.log(data)
+	const onSubmit = () => console.log('hello')
 
-	console.log(watch('example'), errors)
+	console.log('its working', watch('example'), errors)
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)} className="mc__form">
 			<input
-				{...register('addCookie')}
-				className="mc__input"
+				{...register('name')}
+				className="mc__input mc__input--name"
 				placeholder="Input a cookie name!"
 			/>
 
-			<button type="submit">Add a cookie!</button>
+			<input
+				{...register('value')}
+				className="mc__input mc__input--value"
+				placeholder="Input the cookie's value!"
+			/>
+
+			<button
+				className="mc__button mc__button--submit"
+				type="submit"
+				onSubmit={onSubmit}
+			>
+				Add a cookie!
+			</button>
 		</form>
 	)
 }
@@ -35,10 +47,10 @@ export function Header() {
 		<header className="mc__header">
 			<h1 className="mc__h1">Mini Cookies</h1>
 			<p className="mc_desc">
-				Mini Cookies is a simple and minimalistic cookie management tool.
+				Mini Cookies is a minimalist cookie management tool written in
+				Typescript for your website's JavaScript needs.
 			</p>
-			<h2>Try it out!</h2>
-			<AddCookieForm />
+			<h2 className="mc__h2">Try it out!</h2>
 		</header>
 	)
 }
@@ -50,8 +62,11 @@ export default function App() {
 	console.log({ cookies })
 	return (
 		<main className="mc">
-			<CookieEmoji />
-			<Header />
+			<section className="mc__section mc__section--display">
+				<CookieEmoji />
+				<Header />
+				<AddCookieForm />
+			</section>
 		</main>
 	)
 }
