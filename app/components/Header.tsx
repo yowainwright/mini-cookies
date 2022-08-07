9a0006a30c93d824b2ab73729f07e50dfd01220f
yowainwright <yowainwright@gmail.com>
2022-08-06 23:03:41 -0700

    feat: updates badges
    modified:   app/components/Header.tsx
    new file:   app/components/badges.mdx
    modified:   app/components/readme.mdx
    renamed:    docs/assets/index.5d416f02.js -> docs/assets/index.f24e532a.js
    modified:   docs/index.html

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+import Badges from './badges.mdx';
 
 export function Header() {
 	return (
@@ -6,6 +8,11 @@ export function Header() {
 			<h1 className="mc__h1">Mini Cookies</h1>
 			<p className="mc__desc mc__desc--center">
 				A mini JS Document.cookie manager to help you write your cookies right! <span>🎯</span>
+			</p>
+			<p className="mc__desc mc__desc--center">
+				<MDXProvider>
+					<Badges />
+				</MDXProvider>
 			</p>
 		</header>
 	)
