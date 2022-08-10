341daa18f6f15c54fc24e753ef2c6fac46796750
yowainwright <yowainwright@gmail.com>
2022-08-09 18:33:30 -0700

    fix: add slugs to headings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import gfm from 'remark-gfm';
+import slug from 'rehype-slug';
 
 export default defineConfig(async () => {
 const mdx = await import('@mdx-js/rollup');
@@ -14,6 +15,7 @@ return {
       mdx.default({
         providerImportSource: "@mdx-js/react",
         remarkPlugins: [gfm],
+        rehypePlugins: [slug],
       })
   ],
     define: {
