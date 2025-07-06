ebd61b8a8574261aeb6afdb18ca02682f3af3e8a
Jeff Wainwright <1074042+yowainwright@users.noreply.github.com>
2025-07-05 21:40:39 -0700

    chore: updates tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ["src/**/*.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
+  entry: ['src/**/*.ts'],
+  format: ['esm', 'cjs'],
+  dts: {
+    entry: ['src/mini-cookies.ts', 'src/state-manager.ts', 'src/types.ts', 'src/utils.ts'],
+    resolve: true
+  },
   clean: true,
   splitting: true,
   treeshake: true,
   minify: false,
   sourcemap: true,
-  target: "esnext",
-  outDir: "dist",
+  target: 'esnext',
+  outDir: 'dist',
   bundle: false,
   outExtension({ format }) {
     return {
-      js: format === "cjs" ? ".cjs" : ".js",
-    };
-  },
-});
+      js: format === 'cjs' ? '.cjs' : '.js'
+    }
+  }
+})
